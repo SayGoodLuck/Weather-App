e40f586d32cfdc3a97b7f3708a71770262e7ced1
Konstantin Gavrilenko <saaygoodluck@gmail.com>
2021-06-11 18:23:43 +0200

    error fix. added icons and more info

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,17 +3,24 @@ package sample;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
 
 public class Controller {
 
   private static final String API_KEY = "900ea254740cee343805d46750147cdc";
+
+  private String defaultCity = "Kyiv";
+  private String metrics = "\u2103";
 
   @FXML private Button getData;
 
@@ -29,43 +36,62 @@ public class Controller {
 
   @FXML private Text temp_pressure;
 
+  @FXML private Text temp_city;
+
+  @FXML public ImageView weather_condition;
+
   @FXML
   void initialize() {
+
+    getContent(defaultCity);
+
     getData.setOnAction(
         event -> {
-          String getUserCity = city.getText().trim();
-          if (!getUserCity.equals("")) {
-            String output =
-                getUrlContent(
-                    "https://api.openweathermap.org/data/2.5/weather?q="
-                        + getUserCity
-                        + "&appid="
-                        + API_KEY
-                        + "&units=metric");
-
-            if (!output.isEmpty()) {
-              JSONObject json = new JSONObject(output);
-              temp_info.setText("Temperature: " + json.getJSONObject("main").getDouble("temp"));
-              temp_feels.setText(
-                  "Feels like: " + json.getJSONObject("main").getDouble("feels_like"));
-              temp_max.setText("Maximum: " + json.getJSONObject("main").getDouble("temp_max"));
-              temp_min.setText("Minimum: " + json.getJSONObject("main").getDouble("temp_min"));
-              temp_pressure.setText(
-                  "Pressure: " + json.getJSONObject("main").getDouble("pressure"));
-            }
-            System.out.println(output);
-          }
+          String userCity = city.getText().trim();
+          getContent(userCity);
         });
   }
 
+  public void getContent(String city) {
+
+    if (!city.equals("")) {
+      String output =
+          getUrlContent(
+              "https://api.openweathermap.org/data/2.5/weather?q="
+                  + city
+                  + "&appid="
+                  + API_KEY
+                  + "&units=metric");
+
+      if (!output.isEmpty()) {
+        var json = new JSONObject(output);
+        temp_info.setText(json.getJSONObject("main").getInt("temp") + metrics);
+        temp_feels.setText("Feels like: " + json.getJSONObject("main").getDouble("feels_like"));
+        temp_max.setText("Max: " + json.getJSONObject("main").getInt("temp_max") + metrics);
+        temp_min.setText("Min: " + json.getJSONObject("main").getInt("temp_min") + metrics);
+        temp_pressure.setText("Pressure: " + json.getJSONObject("main").getInt("pressure"));
+        temp_city.setText(city + ", " + json.getJSONObject("sys").getString("country"));
+
+        if (json.has("weather")) {
+          var result = json.getJSONArray("weather");
+          var iconCode = result.getJSONObject(0).getString("icon");
+          var image = new Image("http://openweathermap.org/img/w/" + iconCode + ".png");
+          weather_condition.setImage(image);
+        }
+
+      }
+      System.out.println(output);
+    }
+  }
+
   private static String getUrlContent(String urlAdress) {
-    StringBuffer content = new StringBuffer();
+    var content = new StringBuilder();
 
     try {
       URL url = new URL(urlAdress);
       URLConnection urlConn = url.openConnection();
 
-      BufferedReader bufferedReader =
+      var bufferedReader =
           new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
       String line;
       while ((line = bufferedReader.readLine()) != null) {
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,9 +14,11 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Weather");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1920, 1080));
         primaryStage.setResizable(false);
+        primaryStage.setMaximized(true);
         primaryStage.show();
+
     }
 
 
